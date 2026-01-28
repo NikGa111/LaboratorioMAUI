5427ec4dfdc498ad6727055657b29aa74715b7e9
Nik <nik1.inginf@gmail.com>
2026-01-28 15:42:04 +0100

    Convertitore
    Converte da franchi a euro

---
+++ AppConvertitore/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace AppConvertitore
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConverti_Clicked(object sender, EventArgs e)
+        {
+            string valoreImporto = entConversione.Text;
+
+            double franchi = double.Parse(valoreImporto);
+
+            double euro = franchi * 0.92;
+        }
+    }
+
+}
