3e36141d2c984e3b058317f7063cd93b89929a82
Nik <nik1.inginf@gmail.com>
2026-02-27 16:25:18 +0100

    QuizApp

--- QuizApp/MainPage.xaml.cs
+++ QuizApp/MainPage.xaml.cs
@@ -1,12 +1,17 @@
-﻿namespace QuizApp
+﻿using QuizApp.Models;
+
+namespace QuizApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        private List<QuestionBase> _questions = new List<QuestionBase>();
+        private int _currentQuestionIndex = 0;
+        private int _score = 0;
         public MainPage()
         {
             InitializeComponent();
+            _questions.Add(new TrueFalseQuestion("Il C# è un linguaggio a oggetti?", 10, true));
+            _questions.Add(new TrueFalseQuestion("Python è un linguaggio compilato?", 0, true));
         }
 
         private void OnAnswerClicked(object sender, EventArgs e)
